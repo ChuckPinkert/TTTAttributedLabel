a012d253ed594ea6a126ed851e02d4891570789c
Chuck Pinkert <chuck.pinkert@sqor.com>
2015-04-23 09:37:02 -0700

    add linkwith attributes

--- NativeLibrary/TTTAttributedLabel/TTTAttributedLabelSDK.linkwith.cs
+++ NativeLibrary/TTTAttributedLabel/TTTAttributedLabelSDK.linkwith.cs
@@ -1,4 +1,4 @@
 using System;
 using ObjCRuntime;
 
-[assembly: LinkWith ("TTTAttributedLabelSDK.a", LinkTarget.Simulator | LinkTarget.ArmV7, SmartLink = true, ForceLoad = true)]
+[assembly: LinkWith ("TTTAttributedLabelSDK.a", LinkTarget.Simulator | LinkTarget.ArmV7 | LinkTarget.Arm64, LinkerFlags = "-ObjC", SmartLink = true, ForceLoad = true)]
